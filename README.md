d6936dbc11e9f3aa7d9267a6dccc3004ac17ca8d
BlitzKraig <craig@blitzkraig.com>
2021-04-23 16:48:32 +0100

    Added initial pack impl

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # SoundBoard Pack - Blitz Community Pack
 
+![Community Pack Release](https://github.com/BlitzKraig/fvtt-SoundBoard-BlitzCommunityPack/workflows/SoundBoard-BlitzCommunityPack%20Release/badge.svg)
+![Latest Release Download Count](https://img.shields.io/github/downloads/BlitzKraig/fvtt-SoundBoard-BlitzCommunityPack/latest/soundboard-communitypack-release.zip)
+
 [![ko-fi](https://www.ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/Q5Q01YIEJ)
 
 Community contributable sounds pack for use with SoundBoard in FVTT
@@ -15,6 +18,5 @@ Please create a github issue, or contact me on Discord at Blitz#6797
 ## Manifest
 
 `https://raw.githubusercontent.com/BlitzKraig/fvtt-SoundBoard-BlitzCommunityPack/master/module.json`
-
 
 ## [Release Notes](./CHANGELOG.md)
--- importpack.js
+++ importpack.js
@@ -1,5 +1,29 @@
-Hooks.once('SBPackageManagerReady', ()=>{
-    // User contributions should be added here. Please include any valid licensing information in your pack folder
+Hooks.once('SBPackageManagerReady', () => {
+
+    // User contributions should be added here. Please include any valid licensing information in your pack folder, and link to the licensing info using `licenseUrl`
     // You can separate your work into multiple packs if you want, allowing users to enable and disable packs through settings
-    SoundBoard.packageManager.addSoundPack('Rive Pack','modules/SoundBoard-BlitzCommunityPack/bundledAudio/Rive/');
+
+    const moduleName = 'SoundBoard-BlitzCommunityPack',
+        moduleLink = 'https://github.com/BlitzKraig/fvtt-SoundBoard-BlitzCommunityPack';
+
+    SoundBoard.packageManager.addSoundPack(game.i18n.localize('SBCOMMUNITY.Rive.painCombat.title'), 'modules/SoundBoard-BlitzCommunityPack/communityAudio/Rive/PainCombat', moduleName, {
+        description: game.i18n.localize('SBCOMMUNITY.Rive.painCombat.description'),
+        licenses: [{
+            licenseUrl: 'https://raw.githubusercontent.com/BlitzKraig/fvtt-SoundBoard-BlitzCommunityPack/master/communityAudio/Rive/LICENSE',
+            licenseType: 'CC0',
+            licenseDescription: game.i18n.localize('SBCOMMUNITY.Rive.licenseDescription')
+        }],
+        author: 'Rive',
+        link: moduleLink
+    });
+    SoundBoard.packageManager.addSoundPack(game.i18n.localize('SBCOMMUNITY.Rive.hideousLaughter.title'), 'modules/SoundBoard-BlitzCommunityPack/communityAudio/Rive/HideousLaughter', moduleName, {
+        description: game.i18n.localize('SBCOMMUNITY.Rive.hideousLaughter.description'),
+        licenses: [{
+            licenseUrl: 'https://raw.githubusercontent.com/BlitzKraig/fvtt-SoundBoard-BlitzCommunityPack/master/communityAudio/Rive/LICENSE',
+            licenseType: 'CC0',
+            licenseDescription: game.i18n.localize('SBCOMMUNITY.Rive.licenseDescription')
+        }],
+        author: 'Rive',
+        link: moduleLink
+    });
 });
